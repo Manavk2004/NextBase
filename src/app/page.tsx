4160a7ec35816e58f09a99a561d39ba98ea3cd07
Manavk2004 <mjkamdar04@gmail.com>
2025-12-22 14:32:11 -0500

    Setup done

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className='text-red-800 font-extrabold text-5xl'>
+      Hello World
+    </div>
+  )
+}
+
+export default Page
