4bb2fc526ef20fd4468c4ebcdbd80c41e373acdc
Manavk2004 <mjkamdar04@gmail.com>
2025-12-22 20:00:37 -0500

    Switched to prisma 6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 
+import prisma from '@/lib/db'
 import React from 'react'
 
-const Page = () => {
+const Page = async () => {
+  const users = await prisma.user.findMany()
   return (
     <div className='text-red-800 font-extrabold text-5xl'>
-      Hello World
+      {JSON.stringify(users)}
     </div>
   )
 }
