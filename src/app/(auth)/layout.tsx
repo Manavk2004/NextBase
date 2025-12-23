cc1e545d61f80e942fe53b6846d260df2c439e59
Manavk2004 <mjkamdar04@gmail.com>
2025-12-23 16:56:02 -0500

    Theme and Styling

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import AuthLayout from './login/components/auth-layout'
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <AuthLayout>
+        {children}
+    </AuthLayout>
+        
+  )
+}
+
+export default Layout
