2bf9291270d0ffce7f00da4ebda0b1c44ccdcec8
Manavk2004 <mjkamdar04@gmail.com>
2026-01-02 10:45:39 -0500

    Payments

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,8 @@
-import { betterAuth } from "better-auth"
+import { checkout, polar, portal } from "@polar-sh/better-auth"
+import { betterAuth, check } from "better-auth"
 import { prismaAdapter } from "better-auth/adapters/prisma"
 import prisma from "./db"
-
+import { polarClient } from "./polar"
     
 
 export const auth = betterAuth({
@@ -11,6 +12,25 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
         autoSignIn: true
-    }
+    },
+    plugins: [
+        polar({
+            client: polarClient,
+            createCustomerOnSignUp: true,
+            use: [
+                checkout({
+                    products: [
+                        {
+                            productId: "8f5cfd27-a746-4202-bbaf-b720ab6a8afb",
+                            slug: "Nextbase-Pro"
+                        }
+                    ],
+                    successUrl: process.env.POLAR_SUCCESS_URL,
+                    authenticatedUsersOnly: true
+                }),
+                portal()
+            ]
+        })
+    ]
 })
 
